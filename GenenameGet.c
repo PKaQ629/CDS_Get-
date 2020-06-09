43757264e505d7b55b6f4111c6b0ed25accf3573
dachen-726 <66107639+dachen-726@users.noreply.github.com>
2020-06-09 17:59:13 +0800

    笪宸
    三个C程序、一个动态库、一个静态库、一个Makefile文件

---
+++ GenenameGet.c
@@ -0,0 +1,18 @@
+#include "cds.h"
+
+char *genename(FILE *fp){
+  int i, j=0, flag=0;
+  char b[100];
+  char *name = (char *) malloc(100 * sizeof(char));
+  for (i=0;i<100;i++) name[i] = '\0';
+  while((fgets(b,100,fp)) != NULL){
+    if (strstr(b,"/gene")) break;
+  }
+  for(i=0; b[i] != '\0'; i++){
+    if(flag == 1) name[j++]=b[i];
+    if(b[i] == '=') flag++;
+    if(flag == 2) break;
+  }
+  name[j-1]='\0';
+  return name;
+}
